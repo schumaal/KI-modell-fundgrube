2b6fb06e86991de17ab6265a4377e9bbaff525c6
schumaal <schumaal@katharineum.de>
2026-03-12 13:57:48 +0100

    Update Fundgrube.py

--- Fundgrube.py
+++ Fundgrube.py
@@ -12,7 +12,7 @@ IMG_SIZE = 224
 # =================== SUPABASE VERBINDUNG =================
 # =========================================================
 
-SUPABASE_URL = "ajybcyjgvvmcnwujsvnz"
+SUPABASE_URL = "https://ajybcyjgvvmcnwujsvnz.supabase.co"
 SUPABASE_KEY = "sb_publishable_uu46hNj9YnnoDsZV9-I2Rw_IUf-SLbe"
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
