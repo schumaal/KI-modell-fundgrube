3c98219f372cb97432bba9acbb6462ae50ebabea
schumaal <schumaal@katharineum.de>
2026-04-30 13:14:16 +0200

    Update klamotten.py

--- klamotten.py
+++ klamotten.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 from ultralytics import YOLO
+from ultralytics import YOLO
 
 # =========================================================
 # =================== MODEL (YOLOv8) ======================
